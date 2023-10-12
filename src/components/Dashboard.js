62dc8f7d8f4a16baa694a8e371f031461c4435b7
Eleftherios Merkouriou <lef_merk@yahoo.gr>
2023-10-12 15:14:15 +0300

    fix: Added another component in Dashboard to display the polls

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import PollList from "./PollList";
 
 export default function Dashboard() {
     const userLoggedIn = useSelector(state => state.authedUser.id);
@@ -16,31 +16,11 @@ export default function Dashboard() {
         <div className="flex flex-col gap-y-5 justify-center items-center max-w-4xl m-auto mt-4">
             <div className="flex flex-col rounded-md border border-zinc-300 w-full">
                 <h2 className="flex justify-center text-2xl font-semibold border-b border-zinc-300 py-2">New Questions</h2>
-                <div className="flex flex-wrap gap-4 p-3">
-                    {newQ.map(item => 
-                        <div key={item.id} className="rounded-md border border-zinc-300 w-60 h-32 flex flex-col justify-between items-center p-2">
-                            <div className="flex flex-col items-center">
-                                <span className="text-base font-semibold">{item.author}</span>
-                                <span className="text-zinc-400">{new Date(item.timestamp).toLocaleString('el-GR', { timeZone: 'UTC' })}</span>
-                            </div>
-                            <Link to={'/question/' + item.id} className="text-center text-emerald-600 w-full p-2 border border-emerald-600 rounded-md text-sm hover:bg-emerald-600 hover:text-white">Show</Link>
-                        </div>
-                    )}
-                </div>
+                <PollList questions={newQ} />
             </div>
             <div className="flex flex-col rounded-md border border-zinc-300 w-full">
                 <h2 className="flex justify-center text-2xl font-semibold border-b border-zinc-300 py-2">Done</h2>
-                <div className="flex flex-wrap gap-4 p-3">
-                    {doneQ.map(item => 
-                        <div key={item.id} className="rounded-md border border-zinc-300 w-60 h-32 flex flex-col justify-between items-center p-2">
-                            <div className="flex flex-col items-center">
-                                <span className="text-base font-semibold">{item.author}</span>
-                                <span className="text-zinc-400">{new Date(item.timestamp).toLocaleString('el-GR', { timeZone: 'UTC' })}</span>
-                            </div>
-                            <Link to={'/question/' + item.id} className="text-center text-emerald-600 w-full p-2 border border-emerald-600 rounded-md text-sm hover:bg-emerald-600 hover:text-white">Show</Link>
-                        </div>
-                    )}
-                </div>
+                <PollList questions={doneQ} />
             </div>
         </div>
     )
---
+++ src/components/PollList.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+export default function PollList({questions}) {
+    return (
+        <div className="flex flex-wrap gap-4 p-3">
+            {questions.map(item => 
+                <div key={item.id} className="rounded-md border border-zinc-300 w-60 h-32 flex flex-col justify-between items-center p-2">
+                    <div className="flex flex-col items-center">
+                        <span className="text-base font-semibold">{item.author}</span>
+                        <span className="text-zinc-400">{new Date(item.timestamp).toLocaleString('el-GR', { timeZone: 'UTC' })}</span>
+                    </div>
+                    <Link to={'/question/' + item.id} className="text-center text-emerald-600 w-full p-2 border border-emerald-600 rounded-md text-sm hover:bg-emerald-600 hover:text-white">Show</Link>
+                </div>
+            )}
+        </div>
+    );
+}
